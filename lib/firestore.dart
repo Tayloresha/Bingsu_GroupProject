d3268ad9a2b5de0a44954c2f35994309e9e70f2d
Nora <116816965+noraalissa13@users.noreply.github.com>
2024-06-27 11:24:22 +0800

    Update firestore.dart

--- lib/firestore.dart
+++ lib/firestore.dart
@@ -1,17 +1,22 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:family_tracker/member.dart';
 
+// Service class to handle Firestore database operations
 class FirestoreService {
+  // Instance of FirebaseFirestore
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
+  // Stream to get real-time updates of family members collection
   Stream<QuerySnapshot> getFamilyMembers() {
     return _db.collection('family_members').snapshots();
   }
 
+  // Function to add a new family member to the collection
   Future<void> addFamilyMember(Member member) {
     return _db.collection('family_members').add(member.toMap());
   }
 
+  // Function to update an existing family member's details
   Future<void> updateFamilyMember(Member member) {
     return _db
         .collection('family_members')
@@ -19,6 +24,7 @@ class FirestoreService {
         .update(member.toMap());
   }
 
+  // Function to delete a family member from the collection
   Future<void> deleteMember(String docID) async {
     await _db.collection('family_members').doc(docID).delete();
   }
