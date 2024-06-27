041173a6a08332de02e82065c88c0f5f748b1b52
Nora <116816965+noraalissa13@users.noreply.github.com>
2024-06-27 11:32:47 +0800

    Update nav_bar.dart

--- lib/nav_bar.dart
+++ lib/nav_bar.dart
@@ -1,33 +1,39 @@
 import 'package:flutter/material.dart';
 
+// Stateless widget for the bottom navigation bar
 class NavBar extends StatelessWidget {
-  final int currentIndex;
-  final Function(int) onTabTapped;
+  final int currentIndex; // Current selected tab index
+  final Function(int) onTabTapped; // Function to handle tab tap events
 
-  const NavBar(
-      {super.key, required this.currentIndex, required this.onTabTapped});
+  // Constructor to initialize NavBar with required parameters
+  const NavBar({
+    super.key, 
+    required this.currentIndex, 
+    required this.onTabTapped
+  });
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      currentIndex: currentIndex,
-      onTap: onTabTapped,
-      backgroundColor: const Color.fromARGB(255, 116, 56, 183),
-      selectedItemColor: Colors.white,
-      unselectedItemColor: Colors.white.withOpacity(0.7),
-      showUnselectedLabels: false,
+      currentIndex: currentIndex, // Set the current active tab index
+      onTap: onTabTapped, // Handle tab tap events
+      backgroundColor: const Color.fromARGB(255, 116, 56, 183), // Background color of the navigation bar
+      selectedItemColor: Colors.white, // Color for selected tab item
+      unselectedItemColor: Colors.white.withOpacity(0.7), // Color for unselected tab items
+      showUnselectedLabels: false, // Hide labels for unselected tab items
       items: const [
+        // List of bottom navigation bar items
         BottomNavigationBarItem(
-          icon: Icon(Icons.calendar_today),
-          label: 'Schedule',
+          icon: Icon(Icons.calendar_today), // Icon for Schedule tab
+          label: 'Schedule', // Label for Schedule tab
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.assignment),
-          label: 'Reminders',
+          icon: Icon(Icons.assignment), // Icon for Reminders tab
+          label: 'Reminders', // Label for Reminders tab
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.people),
-          label: 'Members',
+          icon: Icon(Icons.people), // Icon for Members tab
+          label: 'Members', // Label for Members tab
         ),
       ],
     );
